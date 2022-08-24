767ffbd8898aec910f963ca6fae3f290b80e9e49
Jonax77 <961843227@qq.com>
2022-08-23 21:20:31 -0700

    prepare for deploy

--- truffle-config.js
+++ truffle-config.js
@@ -22,6 +22,8 @@
 //
 // const fs = require('fs');
 // const mnemonic = fs.readFileSync(".secret").toString().trim();
+const HDWalletProvider = require("@truffle/hdwallet-provider");
+const keys = require("./keys.json");
 
 module.exports = {
     /**
@@ -45,6 +47,21 @@ module.exports = {
             host: "127.0.0.1", // Localhost (default: none)
             port: 7545, // Standard Ethereum port (default: none)
             network_id: "*", // Any network (default: none)
+        },
+        ropsten: {
+            provider: () =>
+                new HDWalletProvider({
+                    mnemonic: {
+                        phrase: keys.MNEMONIC,
+                    },
+                    providerOrUrl: keys.INFURA_ROPSTEN_URL,
+                    addressIndex: 0,
+                }),
+            network_id: 3,
+            gas: 5500000,
+            gasPrice: 20000000000,
+            confirmations: 2,
+            timeoutBlocks: 200,
         },
         //
         // An additional network, but with some advanced options…
